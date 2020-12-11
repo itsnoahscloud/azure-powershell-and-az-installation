60d6c5c09c59b4806ab615c442bd4bec62fb65fa
itsnoahscloud <itsnoahscloud@gmail.com>
2020-12-11 23:54:26 +1000

    Create azure-powershell-and-az-installation.ps1

---
+++ azure-powershell-and-az-installation.ps1
@@ -0,0 +1,17 @@
+# Update msonline
+update-module -name msonline
+
+# Install AzModule
+# Update PowerShellGet
+update-Module -Name PowerShellGet
+
+# Install AzModule
+if ($PSVersionTable.PSEdition -eq 'Desktop' -and (Get-Module -Name AzureRM -ListAvailable)) {
+    Write-Warning -Message ('Az module not installed. Having both the AzureRM and ' +
+      'Az modules installed at the same time is not supported.')
+} else {
+    Install-Module -Name Az -AllowClobber -Scope CurrentUser
+}
+
+# Install or update Azure CLI
+Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
